3b6210471c7ca4a907f1bbe72f63313e4fd7e717
swgem <eric_h_y@hotmail.com>
2018-02-25 18:02:43 -0300

    Atualiza a tela com um dado FPS

--- Stickiller/inc/Display/DisplayTask.hpp
+++ Stickiller/inc/Display/DisplayTask.hpp
@@ -8,12 +8,21 @@ namespace Display {
 	class DisplayTask : public Task
 	{
 	private:
-		ALLEGRO_DISPLAY * display;
+		const int SCREEN_HEIGHT{ 480 };
+		const int SCREEN_WIDTH{ 640 };
+		const double FPS{ 60 };
+
+		ALLEGRO_DISPLAY *m_display;
+		ALLEGRO_EVENT_QUEUE *m_refresherEvent;
+		ALLEGRO_TIMER *m_refresherTimer;
 
 		void run();
 		void initDisplay();
+		void initRefresher();
+		void refreshScreen();
 
 	public:
 		DisplayTask();
+		~DisplayTask();
 	};
 }
--- Stickiller/src/Display/DisplayTask.cpp
+++ Stickiller/src/Display/DisplayTask.cpp
@@ -3,18 +3,62 @@
 void Display::DisplayTask::run()
 {
 	initDisplay();
+	initRefresher();
+
+	int counter = 0;
+
+	while (true)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(m_refresherEvent, &ev);
+
+		if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			counter += 50;
+			if (counter > 255)
+			{
+				counter = 0;
+			}
+			al_clear_to_color(al_map_rgb(counter, counter, counter));
+
+			refreshScreen();
+		}
+	}
 }
 
 void Display::DisplayTask::initDisplay()
 {
-	display = al_create_display(640, 480);
+	m_display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	al_clear_to_color(al_map_rgb(128, 128, 128));
+	al_clear_to_color(al_map_rgb(0, 0, 0));
 
 	al_flip_display();
 }
 
-Display::DisplayTask::DisplayTask()
+void Display::DisplayTask::initRefresher()
+{
+	m_refresherEvent = al_create_event_queue();
+	m_refresherTimer = al_create_timer(1.0 / FPS);
+
+	al_register_event_source(m_refresherEvent, al_get_timer_event_source(m_refresherTimer));
+	al_start_timer(m_refresherTimer);
+}
+
+void Display::DisplayTask::refreshScreen()
+{
+	al_flip_display();
+}
+
+Display::DisplayTask::DisplayTask() : m_display{ nullptr },
+									  m_refresherEvent{ nullptr },
+								      m_refresherTimer{ nullptr }
 {
 
+}
+
+Display::DisplayTask::~DisplayTask()
+{
+	al_destroy_display(m_display);
+	al_destroy_event_queue(m_refresherEvent);
+	al_destroy_timer(m_refresherTimer);
 }
